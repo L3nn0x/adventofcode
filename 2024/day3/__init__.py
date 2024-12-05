588ae9e00aad8a53bd3865fff1c8d68a24f4c197
l3nn0x <dragon83.super@gmail.com>
2024-12-05 01:49:17 +0100

    fix day3 part2

--- 2024/day3/__init__.py
+++ 2024/day3/__init__.py
@@ -7,7 +7,7 @@ def parse_number(data: str) -> (Optional[int], int):
     while index != len(data) and data[index].isdigit():
         index += 1
     if not data[:index].isdigit() or index > 3:
-        return (None, index)
+        return (None, min(index, 3))
     return (int(data[:index]), index)
 
 # returns result + next element to be parsed
@@ -26,7 +26,7 @@ def parse_mul(data: str) -> (Optional[int], int):
         return (None, index)
     if data[index] != ")":
         return (None, index)
-    return (n1 * n2, index+1)
+    return ((n1, n2), index+1)
 
 def parse_conditional(data: str) -> (Optional[bool], int):
     if data[:4] == "do()":
@@ -40,27 +40,60 @@ def part1(line: str) -> int:
     index = 0
     while index <= len(line):
         res, idx = parse_mul(line[index:])
-        index += idx
+        index += max(idx, 1)
         if res:
             total += res
     return total
 
-def part2(line: str) -> int:
+def part2(enabled: bool, line: str) -> int:
     total = 0
     index = 0
-    enabled = True
-    while index <= len(line):
+    while index < len(line):
         res, idx = parse_mul(line[index:])
-        print(f"{'disabled' if not enabled else 'enabled'} mul check {index} {idx} '{line[index:]}'")
-        index += idx
-        to_enable, idx = parse_conditional(line[index:])
-        print(f"{'disabled' if not enabled else 'enabled'} do  check {index} {idx} '{line[index:]}'")
-        index += idx if idx else 1
+        to_enable, idx2 = parse_conditional(line[index:])
+        index += max(max(idx, idx2), 1)
         if to_enable is not None:
             enabled = to_enable
         if enabled and res:
-            total += res
-    return total
+            total += res[0] * res[1]
+    return enabled, total
 
 def run(part: int, input_data: list[str]):
-    print(sum(map(lambda e: part1(e) if part == 1 else part2(e), input_data[:1])))
+    enabled = True
+    total = 0
+    for line in input_data:
+        if part == 1:
+            total += part1(line)
+        else:
+            enabled, tmp = part2(enabled, line)
+            total += tmp
+    print(total)
+
+def test():
+    assert parse_number("") == (None, 0)
+    assert parse_number("a") == (None, 0)
+    assert parse_number("1") == (1, 1)
+    assert parse_number("123") == (123, 3)
+    assert parse_number("1234") == (None, 3)
+    assert parse_number("1,") == (1, 1)
+
+    assert parse_mul("") == (None, 0)
+    assert parse_mul("a") == (None, 0)
+    assert parse_mul("mul (1,2)") == (None, 0)
+    assert parse_mul("mul(a,1)") == (None, 4)
+    assert parse_mul("mul(1, 1)") == (None, 6)
+    assert parse_mul("mul(16,a)") == (None, 7)
+    assert parse_mul("mul(16,1!") == (None, 8)
+    assert parse_mul("mul(1,2)") == (2, 8)
+    assert parse_mul("mul(123,234)") == (123*234, 12)
+
+    assert parse_conditional("") == (None, 0)
+    assert parse_conditional("a") == (None, 0)
+    assert parse_conditional("do()") == (True, 4)
+    assert parse_conditional("don't()") == (False, 7)
+    assert parse_conditional("do't()") == (None, 0)
+
+    assert part2("abcmulmul(1,2)m[2,4/]don't()do()don't()mul(2,3)do()mul(3,3)") == 1*2+3*3
+
+if __name__ == "__main__":
+    test()
