2fbd32d1da8d34c72eef2b5c92deeb44618c8f56
l3nn0x <dragon83.super@gmail.com>
2024-12-05 02:21:42 +0100

    day4 part 2

--- 2024/day4/__init__.py
+++ 2024/day4/__init__.py
@@ -22,9 +22,28 @@ def explore_letter(array: list[str], x: int, y: int) -> int:
         w += int(explore_letter_inner(array, x, y, (0, -1))) # up
     return w
 
-def run(part: int, input_data: list[str]):
+def part1(input_data: list[str]) -> int:
     total = 0
     for y in range(len(input_data)):
         for x in range(len(input_data[0])):
             total += explore_letter(input_data, x, y)
-    print(total)
+    return total
+
+def explore_xmas(array: list[str], x: int, y: int) -> int:
+    if array[y][x] == "A":
+        if x - 1 >= 0 and y - 1 >= 0 and x + 1 < len(array[0]) and y + 1 < len(array):
+            diag1 = array[y-1][x-1] + array[y][x] + array[y+1][x+1]
+            diag2 = array[y-1][x+1] + array[y][x] + array[y+1][x-1]
+            if (diag1 == "MAS" or diag1 == "SAM") and (diag2 == "MAS" or diag2 == "SAM"):
+                return 1
+    return 0
+
+def part2(input_data: list[str]) -> int:
+    total = 0
+    for y in range(len(input_data)):
+        for x in range(len(input_data[0])):
+            total += explore_xmas(input_data, x, y)
+    return total
+
+def run(part: int, input_data: list[str]):
+    print(part1(input_data) if part == 1 else part2(input_data))
